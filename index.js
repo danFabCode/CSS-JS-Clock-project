12ee9b409668d667999a7de5d29682e378e47582
Daniel Code <125013912+danFabCode@users.noreply.github.com>
2023-02-11 11:11:36 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const secondHand = document.querySelector(".second-hand")
+const minHand = document.querySelector(".min-hand")
+const hourHand = document.querySelector(".hour-hand")
+function setDate(){
+    console.log('hello');
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegree = (seconds/60)*360;
+    secondHand.style.transform=`rotate(${secondsDegree}deg)`;
+
+    const minuteHand = now.getMinutes();
+    const minsDegree = (minuteHand/60)*360;
+    minHand.style.transform=`rotate(${minsDegree}deg)`;
+
+    const hours = now.getHours();
+    const hoursDegree = (hours/60)*360;
+    hourHand.style.transform=`rotate(${hoursDegree}deg)`
+
+
+    
+    console.log(secondsDegree);
+};
+setInterval(setDate, 1000);
